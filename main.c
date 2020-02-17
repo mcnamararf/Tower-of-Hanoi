1befa7b6126e984e0ced820bed8c6fdfe1c741ae
Rob McNamara <mcnamararf@vcu.edu>
2020-02-16 20:57:45 -0500

    Added switch statement

--- main.c
+++ main.c
@@ -3,17 +3,56 @@
 #include <stdio.h>
 #include <math.h>
 
+#define RINGONE '&'
+#define RINGTWO '$'
+#define RINGTHREE '#'
+
+
 void printStart();
+int ringScan (char ringSelect, char ringOne, char ringTwo, char ringThree);
 
 
 int main (void) {
 
 	int numMoves = 0;
 
+    char ringOne, ringTwo, ringThree;
+    char ringSelect;
+
+    ringOne   = '&';
+    ringTwo   = '$';
+    ringThree = '#';
+
 
 	printStart();
 
+    printf("Select ring to move:  \n");
+    scanf ("%c",&ringSelect);
+
+    ringScan(ringSelect,ringOne,ringTwo,ringThree);
+
+
 	return 0;
+}
+
+int ringScan (char ringSelect, char ringOne, char ringTwo, char ringThree) {
+
+    switch (ringSelect){
+        case RINGONE :
+            printf("Selected ring one '%s' \n",ringOne);
+            //break;
+        case RINGTWO :
+            printf("Selected ring two '%s' \n",ringTwo);
+            break;
+        case RINGTHREE :
+            printf("Selected ring three '%s' \n",ringThree);
+            break;
+        default :
+            printf("Not a valid ring \n");
+            return 0;
+    }
+
+    return 1;
 }
 
 void printStart(){
