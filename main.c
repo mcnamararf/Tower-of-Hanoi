a3c0e068425b9ecb3985ef06c9135dd1f50d5532
Rob McNamara <mcnamararf@vcu.edu>
2020-02-16 20:38:12 -0500

    Added tower print start for a game

--- main.c
+++ main.c
@@ -3,9 +3,32 @@
 #include <stdio.h>
 #include <math.h>
 
+void printStart();
 
 
 int main (void) {
 
-	
+	int numMoves = 0;
+
+
+	printStart();
+
+	return 0;
+}
+
+void printStart(){
+
+	printf("\n\n");
+	printf("     1        2         3\n");
+	printf("|        |         |         |\n");
+	printf("|        |         |         |\n");
+	printf("|    |   |    |    |    |    |\n");
+	printf("|   &&&  |    |    |    |    |\n");
+	printf("|   &&&  |    |    |    |    |\n");
+	printf("| $$$$$$ |    |    |    |    |\n");
+	printf("| $$$$$$ |    |    |    |    |\n");
+	printf("|########|    |    |    |    |\n");
+	printf("|########|    |    |    |    |\n");
+	printf("------------------------------\n");
+
 }
